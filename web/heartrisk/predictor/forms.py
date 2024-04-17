27ce8216c824de85046fd420b42c805f6fcc78f2
Abhinandan Vijan <abhinandanvijanus@gmail.com>
2024-04-17 19:55:40 -0400

    updated form , other form in progres

--- web/heartrisk/predictor/forms.py
+++ web/heartrisk/predictor/forms.py
@@ -2,6 +2,16 @@ from django import forms
 from .models import HealthData
 
 class HealthDataForm(forms.ModelForm):
+
+    gender  =  forms.ChoiceField(label='gender', choices=((1, 'Male'), (0, 'Female')), widget=forms.RadioSelect)
+    age = forms.IntegerField(label='Age', min_value=0, max_value=150)
+
+    current_smoker = forms.ChoiceField(label='Current Smoker', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
+    bp_meds = forms.ChoiceField(label='BP Meds', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
+    prevalent_stroke = forms.ChoiceField(label='Prevalent Stroke', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
+    prevalent_hypertension = forms.ChoiceField(label='Prevalent Hypertension', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
+    diabetes = forms.ChoiceField(label='Diabetes', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
+    
     class Meta:
         model = HealthData
         fields = ['gender','age','education','current_smoker','cigs_per_day','bp_meds','prevalent_stroke','prevalent_hypertension',
