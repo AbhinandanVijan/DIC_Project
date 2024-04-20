cc4fa0cb539392f88816ab2a35a585a07137b1f5
Abhinandan Vijan <abhinandanvijanus@gmail.com>
2024-04-20 13:24:27 -0400

    form fixed and UI updated

--- web/heartrisk/predictor/forms.py
+++ web/heartrisk/predictor/forms.py
@@ -3,16 +3,17 @@ from .models import HealthData
 
 class HealthDataForm(forms.ModelForm):
 
-    gender  =  forms.ChoiceField(label='gender', choices=((1, 'Male'), (0, 'Female')), widget=forms.RadioSelect)
-    age = forms.IntegerField(label='Age', min_value=0, max_value=150)
+    # gender  =  forms.ChoiceField(label='gender', choices=((1, 'Male'), (0, 'Female')), widget=forms.RadioSelect)
+    # age = forms.IntegerField(label='Age', min_value=0, max_value=150)
 
-    current_smoker = forms.ChoiceField(label='Current Smoker', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
-    bp_meds = forms.ChoiceField(label='BP Meds', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
-    prevalent_stroke = forms.ChoiceField(label='Prevalent Stroke', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
-    prevalent_hypertension = forms.ChoiceField(label='Prevalent Hypertension', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
-    diabetes = forms.ChoiceField(label='Diabetes', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
+    # current_smoker = forms.ChoiceField(label='Current Smoker', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
+    # bp_meds = forms.ChoiceField(label='BP Meds', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
+    # prevalent_stroke = forms.ChoiceField(label='Prevalent Stroke', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
+    # prevalent_hypertension = forms.ChoiceField(label='Prevalent Hypertension', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
+    # diabetes = forms.ChoiceField(label='Diabetes', choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
     
     class Meta:
         model = HealthData
         fields = ['gender','age','education','current_smoker','cigs_per_day','bp_meds','prevalent_stroke','prevalent_hypertension',
                   'diabetes','total_cholestrol','sys_bp','dia_bp','bmi','heart_rate','glucose','heart_stroke']
+     
--- web/heartrisk/predictor/views.py
+++ web/heartrisk/predictor/views.py
@@ -7,14 +7,18 @@ def home(request):
     return render(request, 'home.html', {'form': form})
 
 def submit_health_data(request):
+
     if request.method == 'POST':
         form = HealthDataForm(request.POST)
         if form.is_valid():
             # Process the data, save it to the database, run the risk prediction model
             health_data = form.save(commit=False)
+            print("in the view form valid")
             # Assume a function 'predict_risk' exists that takes health_data and returns a prediction
             #prediction = predict_risk(health_data)
             return render(request, 'result.html', {'prediction': 'Yes'})
+        else:
+            print("form not working", form.errors)
     else:
         form = HealthDataForm()
     return render(request, 'home.html', {'form': form})
